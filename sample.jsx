0253af792be010bab82e4642f189ecc2edb7197a
reaper <ahoy@barelyhuman.dev>
2023-08-10 17:31:33 +0530

    update publish config

--- sample.jsx
+++ sample.jsx
@@ -1,9 +1,9 @@
 class Test {
   render() {
     return <div foo="bar" foo:bar={bux}>
-        This is a test.
-        <Test.Name.Foo />
-        {foo.map(() => 'bar')}
-      </div>;
+      This is a test.
+      <Test.Name.Foo />
+      {foo.map(() => 'bar')}
+    </div>;
   }
 }
